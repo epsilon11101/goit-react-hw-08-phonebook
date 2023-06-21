074c37292b8895ad86fc25f50535240e53334c29
epsilon11101 <amirandav1991@gmail.com>
2023-06-21 08:36:19 -0600

    feat: added contact form and aplied generic styles

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -8,50 +8,83 @@ import {
 } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
 import EditOffIcon from "@mui/icons-material/EditOff";
-import { useForm } from "react-hook-form";
+import { set, useForm } from "react-hook-form";
 import css from "./ContactInfo.module.css";
-import { Fullscreen } from "@mui/icons-material";
+import { useEffect, useState } from "react";
 
-const ContactInfo = ({ fullName }) => {
+const ContactInfo = ({ fullName, phoneNumber }) => {
+  const isNewContact = !fullName && !phoneNumber;
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    defaultValues: {
+      fullName: fullName ? fullName : "",
+      phoneNumber: phoneNumber ? phoneNumber : "",
+    },
+  });
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const onEditHandler = () => {
+    setIsEditing((prevState) => !prevState);
+  };
+
+  const submitHandler = (data) => {
+    console.log(data);
+  };
 
   return (
-    <Box className={css.container}>
-      <Card>
+    <Box>
+      <Card className={css.container}>
         <CardHeader
-          className={!variant ? css.cardHeader : css.cardHeader__variant}
+          className={css.cardHeader}
           avatar={
-            <Avatar
-              className={!variant ? css.avatar : css.avatar__variant}
-              aria-label="recipe"
-            >
-              A
+            <Avatar className={css.avatar} aria-label="recipe">
+              {watch("fullName")[0]}
             </Avatar>
           }
           action={
-            <IconButton aria-label="settings">
-              <EditOffIcon
-                className={
-                  !variant ? css.pendingIcon : css.pendingIcon__variant
-                }
-              />
-            </IconButton>
+            !isNewContact && (
+              <IconButton aria-label="settings" onClick={onEditHandler}>
+                {(!isEditing && (
+                  <EditOffIcon className={css.pendingIcon} />
+                )) || <EditIcon className={css.pendingIcon} />}
+              </IconButton>
+            )
           }
-          title={fullName}
+          title={watch("fullName")}
         />
         <CardContent>
-          <form>
-            <input
-              defaultValue={fullName}
-              placeholder="fullName"
-              {...register("fullName", { required: true })}
-            />
-            {errors.fullName && <span>This field is required</span>}
+          <form className={css.form} onSubmit={handleSubmit(submitHandler)}>
+            <Box className={css.inputWrapper}>
+              <label>Full Name</label>
+              <input
+                placeholder="Full Name"
+                {...register("fullName", {
+                  required: true,
+                })}
+                disabled={isNewContact ? false : !isEditing}
+              />
+              {errors.fullName && <span>This field is required</span>}
+            </Box>
+            <Box className={css.inputWrapper}>
+              <label>Phone Number</label>
+              <input
+                placeholder="Phone Number"
+                {...register("phoneNumber", {
+                  required: true,
+                })}
+                disabled={isNewContact ? false : !isEditing}
+              />
+              {errors.phoneNumber && <span>This field is required</span>}
+            </Box>
+
+            <button type="submit" disabled={isNewContact ? false : !isEditing}>
+              {isNewContact ? "Add Contact" : "Save"}
+            </button>
           </form>
         </CardContent>
       </Card>
@@ -60,5 +93,3 @@ const ContactInfo = ({ fullName }) => {
 };
 
 export default ContactInfo;
-
-//TODO:finished this component
